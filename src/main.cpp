edd9efb94949303d66a60e876f0b7bb9d9155130
maksim.uvarenkov <h.uvarenkov@gmail.com>
2025-01-03 23:32:32 +0300

    [DEV] Introducing include-only spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <versioning/ProjectVersion.h>
+#include <spdlog/spdlog.h>
 
 int main() {
 
-	std::cout << "Hello, I am OTUS-HW-2!" << std::endl;
-	std::cout << "Version is: " << otus_hw_2::ProjectVersion::GetVersion() << std::endl;
+	spdlog::info("Hello, I am OTUS-HW-2!");
+	spdlog::info("Version is: [{}]", to_string(otus_hw_2::ProjectVersion::GetVersion()));
+
+	try {
+
+	}
+	catch (const std::exception& what) {
+
+	}
+
+
 	return 0;
 }
